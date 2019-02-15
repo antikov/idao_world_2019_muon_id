97cb43d6d132e8b82d85465d09162bcbdf825c04
antikov <crazytraffic@gmail.com>
2019-02-15 11:15:44 +0500

    initial commit

---
+++ track_2_baseline/baseline.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <iterator>
+#include <vector>
+#include <limits>
+
+#include "parser.h"
+#include "ripped_evaluator/evaluator.h"
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    const std::string MODEL_FILE = "track_2_model.cbm";
+    NCatboostStandalone::TOwningEvaluator evaluator(MODEL_FILE);
+    // Skip header
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cout << std::setprecision(std::numeric_limits<float>::max_digits10);
+    std::cout << "id,prediction\n";
+    std::vector<float> features(N_FEATURES);
+    while (std::cin.good() && std::cin.peek() != EOF) {
+	size_t id;
+	ugly_hardcoded_parse(std::cin, &id, &features);
+	const float prediction = \
+	    evaluator.Apply(features, NCatboostStandalone::EPredictionType::RawValue);
+	std::cout << id << DELIMITER << prediction  << '\n';
+    }
+    return 0;
+}
